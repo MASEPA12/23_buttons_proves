a1560f4217af2ab43c4368907fdebba0bda2aca9
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-03-27 10:46:03 +0200

    55

--- Assets/manager.cs
+++ Assets/manager.cs
@@ -5,37 +5,22 @@ using UnityEngine.UI;
 
 public class manager : MonoBehaviour
 {
-    public Button pinkButton, greenButton, blueButton;
     private Camera _cameraREF;
-    public GameObject ball1;
-    public Material _materialBall1;
 
     private void Start()
     {
-        _materialBall1 = ball1.GetComponent<MeshRenderer>().material;
         _cameraREF = Camera.main;
     }
     public void ChangeSceneColorGreen()
     {
-        _materialBall1.color = Color.green;
         _cameraREF.backgroundColor = Color.gray;
     }
     public void ChangeSceneColorPink()
     {
-        _materialBall1.color = Color.magenta;
-
         _cameraREF.backgroundColor = Color.black;
     }
     public void ChangeSceneColorBlue()
     {
-        _materialBall1.color = Color.green;
-
         _cameraREF.backgroundColor = Color.white;
     }
-
-    /*public void ChangeSceneColor()
-    {
-        Color colorin = Color.green;
-        _cameraREF.backgroundColor = colorin;
-    }*/
 }
